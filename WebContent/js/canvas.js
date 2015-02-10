3b438771b053e6314c0e28f81b42428ba901c209
challer <challer@38cf96a6-39b0-456b-b5b8-b5fd5798d8b1>
2015-02-10 04:15:09 +0000

    배경있는상태에서의 선 긋기
    git-svn-id: svn://jcoolstory.cafe24.com/respo/noex@160 38cf96a6-39b0-456b-b5b8-b5fd5798d8b1

--- WebContent/js/canvas.js
+++ WebContent/js/canvas.js
@@ -6,19 +6,33 @@ var canvasApp = function(){
 	var upX, upY;
 	var moveX, moveY;
 	
+	var canvasId2 = document.getElementById("ImgDraw");
+	var ctx2 = canvasId2.getContext("2d");
+	var imgObj = new Image();
+	imgObj.src = "http://localhost:8080/jcool/img_the_scream.jpg";
+
 	var init = function(){
+		imgObj.addEventListener("load", function(){
+			ctx2.drawImage(imgObj, 0, 0);
+		}, false);
+
 		canvasId.addEventListener("mousedown", mouseDownListener, false);
+	},
+	//꼭지점 점
+	edgeCircle = function(x, y){
+		ctx.beginPath();
+		//1. x좌표, 2. y좌표, 3. 원의반경, 4. 원의시작과 끝, 5. 2 * Math.PI 전체원 그리기, 6. anticlockwise 매개 변수
+		//x, y, radius, startingAngle, endingAngle, antiClockwise
+		ctx.arc(x, y, 3, 0, 2 * Math.PI, true);
+		ctx.fillStyle="yellow";
+		ctx.fill();
 	},
 	//클릭다운시
 	mouseDownListener = function(evt){
 		downX = evt.layerX;
 		downY = evt.layerY;
-		ctx.clearRect(0, 0, canvasId.width, canvasId.height);
-		ctx.beginPath();
-		//1. x좌표, 2. y좌표, 3. 원의반경, 4. 원의시작과 끝, 5. 2 * Math.PI 전체원 그리기, 6. anticlockwise 매개 변수
-		//x, y, radius, startingAngle, endingAngle, antiClockwise
-		ctx.arc(downX, downY, 3, 0, 2 * Math.PI, true);
-		ctx.fill();
+
+		edgeCircle(downX, downY);
 		
 		dragging = true;
 		if(dragging)
@@ -27,19 +41,15 @@ var canvasApp = function(){
 		canvasId.removeEventListener("mousedown", mouseDownListener, false);
 		window.addEventListener("mouseup", mouseUpListener, false);
 		
-		evt.preventDefault ? evt.preventDefault() : evt.returnValue = false;
+		//이벤트실행 취소
+		//evt.preventDefault ? evt.preventDefault() : evt.returnValue = false;
 
 	},
 	//클릭업시
 	mouseUpListener = function(evt){
 		upX = evt.layerX;
 		upY = evt.layerY;
-		
-		ctx.beginPath();
-		//1. x좌표, 2. y좌표, 3. 원의반경, 4. 원의시작과 끝, 5. 2 * Math.PI 전체원 그리기, 6. anticlockwise 매개 변수
-		//x, y, radius, startingAngle, endingAngle, antiClockwise
-		ctx.arc(upX, upY, 3, 0, 2 * Math.PI, true);
-		ctx.fill();
+		edgeCircle(upX, upY);
 		
 		canvasId.addEventListener("mousedown", mouseDownListener, false);
 		window.removeEventListener("mouseup", mouseUpListener, false);
@@ -47,6 +57,10 @@ var canvasApp = function(){
 			dragging = false;
 			window.removeEventListener("mousemove", mouseMoveListener, false);
 		}
+		console.log("downX" + downX);
+		console.log("downY" + downY);
+		console.log("upX" + upX);
+		console.log("upY" + upY);
 
 	},
 	//클릭다운 후 드래그시
@@ -54,13 +68,13 @@ var canvasApp = function(){
 		moveX = evt.layerX;
 		moveY = evt.layerY;
 		
-		//그려질때 드래그선 지우기
-		ctx.clearRect(downX, downY, moveX-downX, moveY-downY);
+		ctx.canvas.width = ctx.canvas.width;
 		
+		edgeCircle(downX, downY);
+		
+		ctx.strokeStyle = "yellow";
 		//드래그선 그리기
 		ctx.strokeRect(downX, downY, moveX-downX, moveY-downY);
-		
-		//영역을 넘고나서 선이 줄어들때 지우기
 		
 	} 
 	init();
