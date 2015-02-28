e0c4cb744511c93328bfc5c8c04454eeab889b6c
webadmin <webadmin@38cf96a6-39b0-456b-b5b8-b5fd5798d8b1>
2015-02-28 09:22:15 +0000

    ajax upload test 추가
    git-svn-id: svn://jcoolstory.cafe24.com/respo/noex@174 38cf96a6-39b0-456b-b5b8-b5fd5798d8b1

---
+++ WebContent/jcool/testSave1.php
@@ -0,0 +1,16 @@
+<?php
+$data = $_POST['imgData'];
+
+$file = "temp/file.png";
+
+$uri =  substr($data,strpos($data,",")+1);
+
+$result = file_put_contents($file, base64_decode($uri));
+
+if ($result){
+echo $file;
+}else
+{
+echo $result;
+}
+?>
