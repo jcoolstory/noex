9528aca3190585fb50d22b5ae44d42657fad862a
webadmin <webadmin@38cf96a6-39b0-456b-b5b8-b5fd5798d8b1>
2015-03-01 02:17:17 +0000

    code 정
    git-svn-id: svn://jcoolstory.cafe24.com/respo/noex@175 38cf96a6-39b0-456b-b5b8-b5fd5798d8b1

--- WebContent/jcool/testSave1.php
+++ WebContent/jcool/testSave1.php
@@ -1,16 +1,13 @@
 <?php
 $data = $_POST['imgData'];
-
 $file = "temp/file.png";
-
 $uri =  substr($data,strpos($data,",")+1);
-
 $result = file_put_contents($file, base64_decode($uri));
 
 if ($result){
-echo $file;
-}else
-{
-echo $result;
+	echo $file;
+}
+else{
+	echo $result;
 }
 ?>
